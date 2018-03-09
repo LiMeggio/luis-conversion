88eadb1464ee65ad4c6a61708a668f96b4a2ac7d
Linda Meggiolaro <linda.meggiolaro92@gmail.com>
2018-03-09 15:28:44 +0000

    Add 2 new commands

--- conversion.py
+++ conversion.py
@@ -2,3 +2,11 @@ def dollars2cents(dollars):
     cent = dollars * 100
     return cent
 
+def gallons21(gallons):
+	liters = gallons* 4.45
+	return liters
+
+
+def moles2atom:
+	atoms= moles*° 6.888
+	return atoms 
